3687a8eea411696d045d8ac3d22463187c7acfb1
AchillesWasonga <aw6963@princeton.edu>
2024-10-26 23:30:50 -0400

    static

--- campdevoices/settings.py
+++ campdevoices/settings.py
@@ -122,6 +122,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
